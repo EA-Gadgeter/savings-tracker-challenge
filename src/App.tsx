c66d265f006eed95c6fe7db5de5aa00b08cd44ff
EA-Gadgeter <emiacevedoposos@gmail.com>
2026-05-07 01:33:44 -0600

    Fixed wouter configuration to deploy on github actions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import "./App.css";
 import HomePage from "./pages/Home/HomePage";
 import GoalDetailPage from "./pages/GoalDetail/GoalDetailPage";
 
+const base = "/savings-tracker-challenge";
+
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={HomePage} />
-      <Route path="/goals/:id" component={GoalDetailPage} />
-    </Switch>
+    <Router base={base}>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/goals/:id" component={GoalDetailPage} />
+      </Switch>
+    </Router>
   );
 }
 
